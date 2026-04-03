b05e20e3db42698b4b557abca82258bac9bb8e0e
Sanskruti Mahato <sanskrutimahato2511@gmail.com>
2026-04-03 23:27:15 +0530

    final deploy fix

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './',
+
   server: {
     port: 5173,
     open: true
   },
+
   build: {
     rollupOptions: {
       output: {
